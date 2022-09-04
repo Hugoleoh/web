6a85ef1c9f065b7a15cdffb533795d2bfe65fa89
Hugo Ribeiro <hugo.damata98@gmail.com>
2022-09-04 02:59:27 -0300

    Vue and Vuetify installation

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,23 @@
 <template>
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <v-app>
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: "App",
+  name: 'App',
+
   components: {
     HelloWorld,
   },
-};
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  data: () => ({
+    //
+  }),
 }
-</style>
+</script>
