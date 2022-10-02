bfb9899b19ac3895a22ec5397d56d687e19a88af
Hugo Ribeiro <hugo.mataribeiro@gmail.com>
2022-10-02 00:42:25 -0300

    Adding vuex for users

--- src/store/modules/users/actions.js
+++ src/store/modules/users/actions.js
@@ -1,1 +1,26 @@
-export default {};
+import Global from "@/constants/Global.js";
+export default {
+  async getProfile(context) {
+    const userId = context.rootGetters.userId;
+    const token = context.rootGetters.token;
+    const response = await fetch(
+      Global.getServerDomain() + `/users/profile/${userId}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + token,
+        },
+      }
+    );
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      const error = new Error(
+        responseData.message || "Failed to fetch profile"
+      );
+      throw error;
+    }
+    context.commit("setUserProfile", responseData);
+  },
+};
